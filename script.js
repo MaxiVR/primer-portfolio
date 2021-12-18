00adec2ba4f05c750b9ecddddbe24f35bd7e04e0
Maxi <maximilianovazquez88@gmail.com>
2021-12-18 17:26:53 -0300

    Agregue funciones de editar

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
 var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
 
-function cambiarParrafo(){
-    let valor = document.getElementById("prueba").value;
-    document.getElementById("textAcercaDe").innerHTML = valor;
-    document.getElementById("prueba").value= "";
-    
-}
-
-function cambiarDatoPerfil(){
+function cambiarDatoPerfil() {
     let nombre = document.getElementById('nombre').value;
     let ubicacion = document.getElementById("ubicacion").value;
     let imagen = document.getElementById("image").value;
     document.getElementById("nomApellido").innerHTML = nombre;
-    document.getElementById("lugar").innerHTML = ubicacion;
-    urlImage = URL.createObjectURL (image);
-    document.getElementById("foto-perfil").setAttribute.src = urlImage;
+    document.getElementById("lugar-perfil").innerHTML = ubicacion;
+    
     document.getElementById("nombre").value = "";
     document.getElementById("ubicacion").value = "";
-    
+
 }
 
-function agregarNuevosCampos(){
+function cambiarParrafo() {
+    let valor = document.getElementById("info-personal").value;
+    document.getElementById("textAcercaDe").innerHTML = valor;
+    document.getElementById("info-personal").value = "";
+
+}
+
+function editarEdu(){
+    let p = document.getElementById(e);
+    let lugar = document.getElementById("institucion").value;
+    let fecha = document.getElementById("periodo").value;
+    p.firstElementChild.firstElementChild.innerHTML = lugar;
+    p.firstElementChild.lastElementChild.innerHTML = fecha;
+    document.getElementById("institucion").value = "";
+    document.getElementById("periodo").value = "";
+}
+
+let e;
+
+function cambiar(elemento){
+    let x = elemento.parentNode.parentNode.parentNode.parentNode.parentNode.id;
+    console.log(x);
+    e = x;
+}
+let card = "cardEdu ";
+
+function agregarCamposEdu() {
     let $contenedor = document.createElement("div");
-    $contenedor.innerHTML = `<div id="card-1">
-    <div class="titulos-edu">
+    card += 1;
+    $contenedor.setAttribute("id", card);
+    $contenedor.setAttribute("class", "card");
+    $contenedor.innerHTML = `
+    <div class="titulos">
         <h5>Lugar-----</h5>
         <h6>Fecha </h6>
     </div>
@@ -33,40 +54,74 @@ function agregarNuevosCampos(){
             <li class="btn-edit">
                 <!-- Button trigger modal -->
                 <button type="button" class="btn btn-open" data-bs-toggle="modal" 
-                    data-bs-target="#Edu-exp">
-                    <a href="#" class="btn-right">
-                        <i class="fa fa-pencil btn"></i>
-                    </a>
+                    data-bs-target="#modal-edu">
+                    <i class="fa fa-pencil" onclick="cambiar(this)"></i>
+
+                    </button>
                 </button>
             </li>
             <li class="btn-edit">
-                <a href="#" class="btn-right" id="btn-thash" onclick="eliminarInfo(this)">
-                    <i class="fa fa-trash-o btn" aria-hidden="true"> </i></a>
+            <button type="button" class="btn-right" id="btn-thash" onclick="eliminarInfo(this)">
+                <i class="fa fa-trash-o btn" aria-hidden="true"> </i>
+            </button>
             </li>
         </ul>
     </div>
-</div>`;
+`;
 
-    let contenedorEdu = document.getElementById("contenedor-titulo");
-    contenedorEdu.insertBefore($contenedor, contenedorEdu.firstElementChild.nextSibling);
-
-    /* let contenedor = document.createElement("container");
-    let titulo1 = document.createElement("h5");
-    let titulo2 = document.createElement("h6");
-    let contenido = document.createTextNode("Lugar");
-    let contenido2 = document.createTextNode("Fecha");
-    titulo1.appendChild(contenido);
-    titulo2.appendChild(contenido2);
-    contenedor.appendChild(titulo1);
-    contenedor.appendChild(titulo2);
-    let nodoPadre = document.getElementById("contenedor-edu");
-    nodoPadre.appendChild(contenedor); */
+    let contenedorEdit = document.getElementById("contenedor-plus");
+    contenedorEdit.parentNode.insertBefore($contenedor, contenedorEdit.firstElementChild.nextSibling.nextSibling);
 }
 
-
-
-function eliminarInfo(e){
-    e.parentNode.parentNode.parentNode.parentNode.remove(e.parentNode.parentNode.parentNode);
+function eliminarInfo(e) {
+    e.parentNode.parentNode.parentNode.parentNode.remove();
 
 }
 
+let cardTrabajo = "cardTrabajo ";
+function agregarCamposTrabajo() {
+    let $contenedor = document.createElement("div");
+    cardTrabajo += 1;
+    $contenedor.setAttribute("id", cardTrabajo);
+    $contenedor.setAttribute("class", "card");
+    $contenedor.innerHTML = ` 
+                    <div class="titulos">
+                        <h5>Empresa</h5>
+                        <h5>Lugar-----</h5>
+                        <h6>Fecha</h6>
+                    </div>
+                    <div class="contenedor-edit">
+                        <ul class="menu-edit">
+                            <li class="btn-edit">
+                                <button type="button" id="btn-info" class="btn btn-open" data-bs-toggle="modal"
+                                    data-bs-target="#exp">
+                                    <i class="fa fa-pencil btn" onclick="cambiar(this)"></i>
+                                </button>
+                            </li>
+                            <li class="btn-edit">
+                                <button type="button" class="btn-right" id="btn-thash" onclick="eliminarInfo(this)">
+                                    <i class="fa fa-trash-o btn" aria-hidden="true"> </i>
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                `
+
+    let contenedorEdit = document.getElementById("contenedor-titulo-trabajo");
+    contenedorEdit.parentNode.insertBefore($contenedor, contenedorEdit.firstElementChild.nextSibling.nextSibling);
+
+}
+
+function editarTrabajo(){
+    let p = document.getElementById(e);
+    console.log(p);
+    let empresa = document.getElementById("empresa").value;
+    let puesto = document.getElementById("puesto").value;
+    let periodo = document.getElementById("periodoTrabajado").value;
+    p.firstElementChild.firstElementChild.innerHTML = empresa;
+    p.firstElementChild.firstElementChild.nextElementSibling.innerHTML = puesto;
+    p.firstElementChild.lastElementChild.innerHTML = periodo;
+    document.getElementById("empresa").value = "";
+    document.getElementById("puesto").value = "";
+    document.getElementById("periodoTrabajado").value = "";
+}
